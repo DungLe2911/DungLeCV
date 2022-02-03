4b616486b80048e6afec774efaa0611ad8eb812e
Richard <lehoangdung29111998@gmail.com>
2022-02-02 23:24:58 -0800

    adding react-router-dom dependencies

--- dunglecv/src/component/NavBar.js
+++ dunglecv/src/component/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import * as FaIcons from 'react-icons/fa';
 
 export default function NavBar(){
-    return(
-        <div className='navBar'>
-            <div className='menuBtn'></div>
-            <div className='navBarItem'></div>
-            <div className='downloadBtn'></div>
-        </div>
-    );
+	return(
+		<div className='navBar'>
+			<div className='menuBtn'>
+				
+			</div>
+			<div className='navBarItem'></div>
+			<div className='downloadBtn'></div>
+		</div>
+	);
 };
