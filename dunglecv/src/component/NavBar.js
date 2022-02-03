1b037ea86dfad06b1f810880ea2722a1d2cd756b
Richard <lehoangdung29111998@gmail.com>
2022-02-03 13:23:21 -0800

    Added dynamic navbar menu items

--- dunglecv/src/component/NavBar.js
+++ dunglecv/src/component/NavBar.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
+import * as BsIcons from 'react-icons/bs'
 import {IconContext} from 'react-icons';
 import { Link } from 'react-router-dom';
-
-import CV from '../documents/DungLeCV.pdf';
 
 
 export default function NavBar(){
 	const [fullMenu, toggleFullMenu] = useState(false);
+	const data = [
+		{icon: <BsIcons.BsPerson/>, label: "lable 1"},
+		{icon: <AiIcons.AiOutlinePhone/>, label: "Contact"}
+	]	
 	const toggleNavBar = () =>{
 		toggleFullMenu(!fullMenu)
 		console.log(fullMenu)
 	}
+	// //componentDidMount()
+	// useEffect(()=>{
+		
+	// },[]);
 	return(
 		<div className={fullMenu? "navBar navBarActive": "navBar"}>
 			<IconContext.Provider value={{ style: {fontSize: '30px'}}}>
@@ -21,11 +28,27 @@ export default function NavBar(){
 						{fullMenu ? <FaIcons.FaTimes/>:<FaIcons.FaBars/>}
 					</div>
 				</Link>
-				{/* <div className='navBarItem'></div> */}
+				<div className='navBarMain'>
+					<div className="navBarItemContainer">
+						{data.map((ele,idx)=>{
+							return(							
+								<Link className='navBarItem' key={idx}>
+									<div style={{marginLeft:'10px', display:'flex', flexDirection:'row', alignItems:'center'}}>
+										{ele.icon}
+										&nbsp;
+										{fullMenu? <div>{ele.label}</div>:null}
+									</div>
+								</Link>
+							)
+						})}
+					</div>
+				</div>
 				<div className='downloadBtn'>
 					<a href='/documents/DungLeCV.pdf' download>
 						<div className='downloadBtnContainer'>
 							<AiIcons.AiOutlineCloudDownload/>
+							&nbsp;
+							{fullMenu?<div>Dung (Richard) Le's CV</div>:null}
 						</div>
 					</a>
 				</div>
