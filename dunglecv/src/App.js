363fde03f312ddcc32109032e137a5319b15afd4
Richard <lehoangdung29111998@gmail.com>
2022-02-02 22:48:16 -0800

    initiallized navBar

--- dunglecv/src/App.js
+++ dunglecv/src/App.js
@@ -1,9 +1,9 @@
 import './style/App.css';
-
+import NavBar from './component/NavBar';
 function App() {
   return (
     <div className="App">
-    
+      <NavBar/>
     </div>
   );
 }
---
+++ dunglecv/src/component/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function NavBar(){
+    return(
+        <div className='navBar'>
+            <div className='menuBtn'></div>
+            <div className='navBarItem'></div>
+            <div className='downloadBtn'></div>
+        </div>
+    );
+};
